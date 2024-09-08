cab87e4e25871ef5eb7efe873be15ac313e2fd06
reallymanya <manyatakkar.01@gmail.com>
2024-09-08 17:57:55 +0530

    Update swapalternate.cpp

--- swapalternate.cpp
+++ swapalternate.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void swapAlternate(int arr[], int size){
     for(int i = 0; i<size; i+= 2){
         if(i+1 < size){
-            swap(arr[i], arr[i+1]);
+            swap(arr[i], arr[i+1]); //temp = arr[i+1], arr[i+1] = arr[i], arr[i] = temp
         }
     }
 }
